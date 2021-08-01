18561353dd37095ebf99ec6e2187e6ab17de90b2
playing-games <playing-games@program>
2021-08-01 04:04:26 +0300

    from program.sh i remove no-history push

--- program.sh
+++ program.sh
@@ -32,15 +32,4 @@ csgo(){
   1
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am playing-games program"
-  git push -f -u origin main
-}
-
 "$@"
